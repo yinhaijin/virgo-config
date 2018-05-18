c23283cddd2c82e4dbe3aa9669c41d4e3ece77ba
hakim <yinhaijin@hotmail.com>
2018-05-18 13:57:44 +0800

    修改忽略项
    修改忽略项

---
+++ src/main/java/com/lunzhiyu/virgo/VirgoApplication.java
@@ -0,0 +1,14 @@
+package com.lunzhiyu.virgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yhj on 2018/3/28.
+ */
+@SpringBootApplication
+public class VirgoApplication {
+    public static void main(String[] args){
+        SpringApplication.run(VirgoApplication.class, args);
+    }
+}
