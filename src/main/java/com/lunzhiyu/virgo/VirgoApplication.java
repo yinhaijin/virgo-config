d0969d98c384036ec65387a73aca5885969b28c5
hakim <yinhaijin@hotmail.com>
2018-05-19 09:39:40 +0800

    基础文件创建
    基础文件创建

--- src/main/java/com/lunzhiyu/virgo/VirgoApplication.java
+++ src/main/java/com/lunzhiyu/virgo/VirgoApplication.java
@@ -2,11 +2,13 @@ package com.lunzhiyu.virgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by yhj on 2018/3/28.
  */
 @SpringBootApplication
+@EnableConfigServer
 public class VirgoApplication {
     public static void main(String[] args){
         SpringApplication.run(VirgoApplication.class, args);
