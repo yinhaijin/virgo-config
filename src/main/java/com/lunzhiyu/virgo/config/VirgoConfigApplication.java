cf1f383a1d76f00e9c91ec1fa7fcda61f1ae94d2
hakim <yinhaijin@hotmail.com>
2018-05-23 14:47:11 +0800

    提交初始化代码
    提交初始化代码

--- src/main/java/com/lunzhiyu/virgo/config/VirgoConfigApplication.java
+++ src/main/java/com/lunzhiyu/virgo/config/VirgoConfigApplication.java
@@ -1,4 +1,4 @@
-package com.lunzhiyu.virgo;
+package com.lunzhiyu.virgo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +9,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
-public class VirgoApplication {
+public class VirgoConfigApplication {
     public static void main(String[] args){
-        SpringApplication.run(VirgoApplication.class, args);
+        SpringApplication.run(VirgoConfigApplication.class, args);
     }
 }
