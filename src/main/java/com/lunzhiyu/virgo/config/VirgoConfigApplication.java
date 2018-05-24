76d5078f0dd84bdc089fea9bf032f53bc85f15e3
hakim <yinhaijin@hotmail.com>
2018-05-24 11:28:45 +0800

    初始化config项目

--- src/main/java/com/lunzhiyu/virgo/config/VirgoConfigApplication.java
+++ src/main/java/com/lunzhiyu/virgo/config/VirgoConfigApplication.java
@@ -3,12 +3,14 @@ package com.lunzhiyu.virgo.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by yhj on 2018/3/28.
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class VirgoConfigApplication {
     public static void main(String[] args){
         SpringApplication.run(VirgoConfigApplication.class, args);
