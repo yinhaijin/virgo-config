4d480a327bd1dac18f5b84efef92cd3180120140
hakim <yinhaijin@hotmail.com>
2018-05-25 08:01:28 +0800

    修改config项目名称

--- src/main/java/com/lunzhiyu/virgo/config/VirgoConfigApplication.java
+++ src/main/java/com/lunzhiyu/virgo/config/VirgoConfigApplication.java
@@ -6,6 +6,17 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
+ * 在application.yml中有个git.uri的配置，目前配置的是https://github.com/yinhaijin/config-repo
+ * 获取git上的资源信息遵循如下规则：
+ * /{application}/{profile}[/{label}]
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
+ *
+ * 例如本例：可使用以下路径来访问{application}-dev.properties：
+ * http://localhost:8040/{application}-dev.properties
+ * http://localhost:8040/{application}/dev
  * Created by yhj on 2018/3/28.
  */
 @SpringBootApplication
